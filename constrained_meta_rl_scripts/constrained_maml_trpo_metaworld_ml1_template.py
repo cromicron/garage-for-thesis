409fca0700135e5811f3737e48cf688f6f71a5bd
cromicron <darionalis@gmail.com>
2024-07-31 16:59:26 +0200

    script template for running ml1 maml experiments with argparse

--- constrained_meta_rl_scripts/constrained_maml_trpo_metaworld_ml1_template.py
+++ constrained_meta_rl_scripts/constrained_maml_trpo_metaworld_ml1_template.py
@@ -1,15 +1,14 @@
+
 #!/usr/bin/env python3
-"""Train Constrained Meta-Rl on metaworld pick-place env"""
-# pylint: disable=no-value-for-parameter
-# yapf: disable
-import click
+"""Train Constrained Meta-RL on metaworld pick-place env"""
+
+import argparse
 import metaworld_constrained as metaworld
 import torch
 
 from garage import wrap_experiment
 from garage.envs import MetaWorldSetTaskEnv
-from garage.experiment import (MetaEvaluator, MetaWorldTaskSampler,
-                               SetTaskSampler)
+from garage.experiment import (MetaEvaluator, MetaWorldTaskSampler, SetTaskSampler)
 from garage.experiment.deterministic import set_seed
 from garage.np.baselines import LinearFeatureBaseline
 from garage.sampler import RaySampler
@@ -18,66 +17,19 @@ from garage.torch.policies import GaussianMLPPolicy
 from garage.trainer import Trainer
 import wandb
 
-
-# yapf: enable
-@click.command()
-@click.option('--env-name', type=str, default='pick-place-v2')
-@click.option('--seed', type=int, default=1)
-@click.option('--epochs', type=int, default=2000)
-@click.option('--rollouts_per_task', type=int, default=10)
-@click.option('--meta_batch_size', type=int, default=25)
-@click.option('--inner_lr', default=1e-4, type=float)
-@click.option('--train_constraint', default=True, type=bool)
-@click.option('--lr_lagrangian', default=3e-1, type=float)
-@click.option('--lagrangian', default=10.0, type=float)
-@click.option('--constraint_mode', default="relative", type=str)
-@click.option('--constraint_size', default=0.03, type=float)
-@click.option('--w_and_b', default=True, type=bool)
 @wrap_experiment(
     snapshot_mode='last',
     name_parameters='passed',
     archive_launch_repo = False,
     use_existing_dir=True,
 )
-def constrained_maml_trpo_metaworld_ml1(
-    ctxt,
-    env_name,
-    seed,
-    epochs,
-    rollouts_per_task,
-    meta_batch_size,
-    inner_lr,
-    train_constraint,
-    lr_lagrangian,
-    lagrangian,
-    constraint_mode,
-    constraint_size,
-    w_and_b
-):
-    """Set up environment and algorithm and run the task.
+def main(ctxt, env_name, seed, epochs, rollouts_per_task, meta_batch_size, inner_lr,
+         train_constraint, lr_lagrangian, lagrangian, constraint_mode, constraint_size, w_and_b):
+    """Set up environment and algorithm and run the task."""
 
-    Args:
-        ctxt (garage.experiment.ExperimentContext): The experiment
-            configuration used by Trainer to create the snapshotter.
-        env_name (str): Name of Meta-World environment to initialize
-            this experiment with.
-        seed (int): Used to seed the random number generator to produce
-            determinism.
-        epochs (int): Number of training epochs.
-        rollouts_per_task (int): Number of rollouts per epoch per task
-            for training.
-        meta_batch_size (int): Number of tasks sampled per batch.
-        inner_lr (float): learning rate to use for the inner TRPO agent.
-            This hyperparameter is typically the one to tune when tuning
-            your MAML.
-        lagrangian (float): Initial value for lagrangian multiplier
-        w_and_b (bool): Whether to log to weights and biases
-
-    """
     set_seed(seed)
-
     ml1 = metaworld.ML1(env_name, constraint_mode=constraint_mode, constraint_size=constraint_size)
-    constructor_args = {"constraint_mode": ml1.constraint_mode, "constraint_size":ml1.constraint_size}
+    constructor_args = {"constraint_mode": ml1.constraint_mode, "constraint_size": ml1.constraint_size}
     tasks = MetaWorldTaskSampler(ml1, 'train', constructor_args=constructor_args)
     env_cl = tasks.sample(1)[0]
     env = env_cl()
@@ -98,8 +50,6 @@ def constrained_maml_trpo_metaworld_ml1(
     policy.register_parameter(
         "lagrangian",
         torch.nn.Parameter(torch.tensor(lagrangian)))
-    # In lagrangian methods, there are two separate advantages
-    # thus two value functions necessary
     value_function = LinearFeatureBaseline(env_spec=env.spec)
     value_function_const = LinearFeatureBaseline(
         env_spec=env.spec,
@@ -116,7 +66,7 @@ def constrained_maml_trpo_metaworld_ml1(
                          max_episode_length=env.spec.max_episode_length,
                          n_workers=meta_batch_size)
 
-    trainer = Trainer(ctxt)
+    trainer = Trainer()
     algo = MAMLTRPO(
         env=env,
         policy=policy,
@@ -141,7 +91,7 @@ def constrained_maml_trpo_metaworld_ml1(
         w_and_b=w_and_b,
     )
     if w_and_b:
-        wandb.init(project=f"constrained-maml-ml1-{env_name}",config={
+        wandb.init(project=f"constrained-maml-ml1-{env_name}", config={
             "inner_rl": inner_lr,
             "meta_batch_size": meta_batch_size,
             "discount": 0.99,
@@ -154,12 +104,26 @@ def constrained_maml_trpo_metaworld_ml1(
             "constraint_mode": constraint_mode,
             "constraint_size": constraint_size,
             "constraint_threshold": 0.001,
-            "environment": "pick-place",
-
+            "environment": env_name,
         })
     trainer.setup(algo, env)
     trainer.train(n_epochs=epochs,
                   batch_size=rollouts_per_task * env.spec.max_episode_length)
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Train Constrained Meta-RL on MetaWorld environments.')
+    parser.add_argument('--env-name', type=str, default='pick-place-v2')
+    parser.add_argument('--seed', type=int, default=1)
+    parser.add_argument('--epochs', type=int, default=2000)
+    parser.add_argument('--rollouts_per_task', type=int, default=10)
+    parser.add_argument('--meta_batch_size', type=int, default=25)
+    parser.add_argument('--inner_lr', type=float, default=1e-4)
+    parser.add_argument('--train_constraint', type=bool, default=True)
+    parser.add_argument('--lr_lagrangian', type=float, default=5e-1)
+    parser.add_argument('--lagrangian', type=float, default=1.0)
+    parser.add_argument('--constraint_mode', type=str, default='relative')
+    parser.add_argument('--constraint_size', type=float, default=0.03)
+    parser.add_argument('--w_and_b', type=bool, default=True)
+    args = parser.parse_args()
 
-constrained_maml_trpo_metaworld_ml1()
+    main(**vars(args))
